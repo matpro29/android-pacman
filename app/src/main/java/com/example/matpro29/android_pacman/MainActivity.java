b9690028b640761113702e706d82e9615deffea2
Mateusz Proskura <matpro29@outlook.com>
2018-11-21 20:05:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/matpro29/android_pacman/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.matpro29.android_pacman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
