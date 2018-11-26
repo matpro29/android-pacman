41f393dd2692fee3ea1befa8daca2ea98fe5e399
Mateusz Proskura <matpro29@outlook.com>
2018-11-26 19:36:46 +0100

    Zyroskop

--- app/src/main/java/com/example/matpro29/android_pacman/MainActivity.java
+++ app/src/main/java/com/example/matpro29/android_pacman/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.matpro29.android_pacman;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView tv1;
+    TextView tv2;
+    TextView tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), 0, null);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        tv1.setText("event.values[0] = " + Float.toString(event.values[0]));
+        tv2.setText("event.values[1] = " + Float.toString(event.values[1]));
+        tv3.setText("event.values[2] = " + Float.toString(event.values[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
